3cf8b2b1297fddf70472358e4579b77691fcea7b
rasmus123d <59487370+RasmusKD@users.noreply.github.com>
2024-12-05 11:46:43 +0100

    update db connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,14 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const uri = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI;
+
         if (!uri) {
             throw new Error('MongoDB URI not defined');
         }
 
-        await mongoose.connect(uri, {
-            useNewUrlParser: true, // Optional in modern versions
-            useUnifiedTopology: true, // Optional in modern versions
-        });
+        await mongoose.connect(uri); // No options needed
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('Database connection failed:', error.message);
